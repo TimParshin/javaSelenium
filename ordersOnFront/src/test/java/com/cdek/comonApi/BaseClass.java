bbcab333c4bf94eb21b9154adf5db70ea1c40ce0
Tim <parshintn@gmail.com>
2019-03-09 14:55:39 +0700

    Законичл поиск накладной

--- ordersOnFront/src/test/java/com/cdek/comonApi/BaseClass.java
+++ ordersOnFront/src/test/java/com/cdek/comonApi/BaseClass.java
@@ -1,9 +1,6 @@
 package com.cdek.comonApi;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -15,51 +12,55 @@ public class BaseClass {
     WebDriver driver;
 
     public void initChromedriver() {
-      System.setProperty("webdriver.chrome.driver", "C://Tools/chromedriver.exe");
-      driver = new ChromeDriver();
-      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.setProperty("webdriver.chrome.driver", "C://Tools/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     public void clickOrder() {
-      driver.findElement(By.linkText("1105673653")).click();
+        driver.findElement(By.linkText("1105673653")).click();
     }
 
     public void findOrder() {
-      //driver.findElement(By.xpath("//*[@id=\"filter_field_orderNumber\"]")).sendKeys("1105673653");
-      WebElement frame = driver.findElement(By.xpath("//iframe[5]"));
+        //driver.findElement(By.xpath("//*[@id=\"filter_field_orderNumber\"]")).sendKeys("1105673653");
+        WebElement frame = driver.findElement(By.xpath("//*[@id[contains(.,'ifrm_menuOrder')]]"));
         driver.switchTo().frame(frame);
-      driver.switchTo().frame("gate");
-      driver.findElement(By.id("filter_field_orderNumber")).sendKeys("110567653");
-      driver.findElement(By.name("vm.filterForm")).submit();
+        driver.switchTo().frame("gate");
+        WebElement orderNumberField = driver.findElement(By.xpath("//*[@id=\"filter_field_orderNumber\"]/div/input"));
+        orderNumberField.sendKeys("110567653");
+        orderNumberField.sendKeys(Keys.ENTER);
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"findButtonOrder\"]"));
+        searchButton.click();
+        //driver.findElement(By.name("vm.filterForm")).submit();
     }
 
     public void openOrderList() {
-      driver.findElement(By.id("overMainHeader")).click();
-      driver.findElement(By.xpath("//*[@id=\"menuDelivery\"]")).click();
+        driver.findElement(By.id("overMainHeader")).click();
+        driver.findElement(By.xpath("//*[@id=\"menuDelivery\"]")).click();
         fixCoveredElement("//*[@id=\"menuOrder\"]");
     }
 
     public void fixCoveredElement(String elementPath) {
         elementPath = elementPath;
         WebElement ele = driver.findElement(By.xpath(elementPath));
-        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
         executor.executeScript("arguments[0].click();", ele);
     }
 
     public void navfrontAuth() {
-      driver.get("https://navfront.qa2.cdek.ru/#/viewLogin");
-      driver.findElement(By.id("login")).clear();
-      driver.findElement(By.id("login")).sendKeys("tester_nsk");
-      driver.findElement(By.id("password")).clear();
-      driver.findElement(By.id("password")).sendKeys("123");
-      driver.findElement(By.id("target")).click();
+        driver.get("https://navfront.qa2.cdek.ru/#/viewLogin");
+        driver.findElement(By.id("login")).clear();
+        driver.findElement(By.id("login")).sendKeys("tester_nsk");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("123");
+        driver.findElement(By.id("target")).click();
     }
 
     public void stopDriver() {
-      driver.quit();
-      String verificationErrorString = verificationErrors.toString();
-      if (!"".equals(verificationErrorString)) {
-        fail(verificationErrorString);
-      }
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
     }
 }
