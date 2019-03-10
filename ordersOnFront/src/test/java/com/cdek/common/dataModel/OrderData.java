99f4a6a5c369054bdef9671f73c38a84be04951a
Tim <parshintn@gmail.com>
2019-03-10 19:58:56 +0700

    Создана модель данных заказа

--- ordersOnFront/src/test/java/com/cdek/common/dataModel/OrderData.java
+++ ordersOnFront/src/test/java/com/cdek/common/dataModel/OrderData.java
@@ -4,19 +4,94 @@ import java.util.Date;
 
 public class OrderData {
 
-    String NumberOrd;
-    String NumberIn;
-    String SellerAddress;
-    String ShipperName;
-    String ShipperAddress;
-    Date DateInvoice;
-    String TaxpayerIdentificationNumber;
-    String PasSer;
-    String PasNum;
-    String DateIssue;
-    String IssuedBy;
-    Date DateOfBirth;
-    String NameEx;
-    Double  WeightBrutto;
-    String Link;
+    String numberOrd;
+    String numberIn;
+    String sellerAddress;
+    String shipperName;
+    String shipperAddress;
+    Date dateInvoice;
+    String taxpayerIdentificationNumber;
+    String pasSer;
+    String pasNum;
+    Date dateIssue;
+    String issuedBy;
+    Date dateOfBirth;
+    String nameEx;
+    Double weightBrutto;
+    String link;
+
+    public String getNumberOrd (String numberOrd) {
+        this.numberOrd = numberOrd;
+        return numberOrd;
+    }
+
+    public String getNumberIn (String numberIn) {
+        this.numberIn = numberIn;
+        return numberIn;
+    }
+
+    public String getSellerAddress (String sellerAddress) {
+        this.sellerAddress = sellerAddress;
+        return sellerAddress;
+    }
+
+    public String getShipperName (String shipperName) {
+        this.shipperName = shipperName;
+        return shipperName;
+    }
+
+    public String getShipperAddress (String shipperAddress) {
+        this.shipperAddress = shipperAddress;
+        return shipperAddress;
+    }
+
+    public Date getDateInvoice(Date dateInvoice) {
+        this.dateInvoice = dateInvoice;
+        return dateInvoice;
+    }
+
+    public String getTaxpayerIdentificationNumber (String taxpayerIdentificationNumber) {
+        this.taxpayerIdentificationNumber = taxpayerIdentificationNumber;
+        return taxpayerIdentificationNumber;
+    }
+
+    public String getPasSer(String pasSer) {
+        this.pasSer = pasSer;
+        return pasSer;
+    }
+
+    public String getPasNum(String pasNum) {
+        this.pasNum = pasNum;
+        return pasNum;
+    }
+
+    public Date getDateIssue(Date dateIssue) {
+        this.dateIssue = dateIssue;
+        return dateIssue;
+    }
+
+    public String getIssuedBy(String issuedBy) {
+        this.issuedBy = issuedBy;
+        return issuedBy;
+    }
+
+    public Date getDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+        return dateOfBirth;
+    }
+
+    public String getNameEx(String nameEx) {
+        this.nameEx = nameEx;
+        return nameEx;
+    }
+
+    public Double getWeightBrutto(Double weightBrutto) {
+        this.weightBrutto = weightBrutto;
+        return weightBrutto;
+    }
+
+    public String getLink(String link) {
+        this.link = link;
+        return link;
+    }
 }
